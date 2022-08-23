58ed3a6d28a30344429c49194b32f4f9d2a97998
FidanAkhmedova <fidan.akhmed.anar98@gmail.com>
2022-08-23 20:55:36 +0400

    commit

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,34 +4,54 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	int clk = 0;
+	Anim animation;
+	private float speed = 200.0f;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("globe.jpg");//task
+		animation = new Anim("favpng_sprite-animation-2d-computer-graphics-video-game.png",5, 2,Animation.PlayMode.LOOP  );
 	}
+
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		float x = (Gdx.graphics.getWidth() - img.getWidth())/2 ;
-		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)) clk++;
-		//if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clk++;
-		Gdx.graphics.setTitle("Clicked" +clk+ "times");
+		ScreenUtils.clear(1, 1, 1, 1);
+		animation.setTime(Gdx.graphics.getDeltaTime());
+
+		float x = 0;
+		float t = 0;
+		float t1 = 0;
+		float t2 = 0;
+		t = animation.getTime();
+
+		if (!animation.getFrame().isFlipX()){
+			x = t*this.speed;
+			if (x >= Gdx.graphics.getWidth()-animation.getFrame().getRegionWidth())
+				animation.getFrame().flip(true, false);
+		}
+
+		if (animation.getFrame().isFlipX()){
+			x -= t*this.speed;
+		}
+
 		batch.begin();
-		batch.draw(img, x, 0);
+		batch.draw(animation.getFrame(), x, 0);
 		batch.end();
 	}
+
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		animation.dispose();
 	}
 }
